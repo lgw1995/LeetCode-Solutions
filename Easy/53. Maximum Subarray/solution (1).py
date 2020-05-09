3a1ba5e07fe2571f5351af4bdc46c4b95658066f
Liugw <liugw01@outlook.com>
2020-05-09 23:27:58 +0800

    Feat: Add #53

---
+++ Easy/53. Maximum Subarray/solution (1).py
@@ -0,0 +1,16 @@
+# 53. Maximum Subarray
+
+# Runtime: 64 ms, faster than 81.39% of Python3 online submissions for Maximum Subarray.
+
+# Memory Usage: 14.7 MB, less than 5.69% of Python3 online submissions for Maximum Subarray.
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        assert len(nums) > 0 
+
+        cur_sum = max_sum = nums[0]
+        for i in range(1, len(nums)):
+            cur_sum = max(nums[i],cur_sum + nums[i])
+            max_sum = max(max_sum,cur_sum)
+        return max_sum
